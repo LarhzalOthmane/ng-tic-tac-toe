11206b06daf5fbdd774067fe40c3a2710a2cd257
Larhzal Othmane <othmane.larhzal@gmail.com>
2021-08-19 19:16:32 +0100

    version 1.0

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+// Dumb component (or UI component)
+@Component({
+  selector: 'app-square',
+  template: `
+      <button nbButton *ngIf="!value">{{ value }}</button>
+      <button nbButton hero status="success" *ngIf="value === 'O'">
+        {{ value }}
+      </button>
+      <button nbButton hero status="info" *ngIf="value === 'X'">
+        {{ value }}
+      </button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'],
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O' | undefined;
+}
